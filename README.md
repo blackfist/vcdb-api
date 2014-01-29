24aeefc15ba0628478dab10cbecfc865e0d7687f
Kevin Thompson <kevin.david.thompson@gmail.com>
2014-01-29 20:10:44 +0000

    initial database connectivity and api method.

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
-vcdb-api
-========
+# vcdb-api
+an API to return JSON data from the VCDB.
 
-an API to return JSON data from the VCDB
+# Methods
+## get\_one
+_get\_one/_
+_get\_one/pretty_
+Used primarily for testing database connectivity. This will return a single document from the database. Use the /pretty option to have
+the output formatted nicer.
--- server.py
+++ server.py
@@ -1,5 +1,20 @@
 import ConfigParser
 import pymongo
+from flask import Flask, render_template, request, redirect, url_for
+import json
+
+api = Flask(__name__)
+
+@api.route('/get_one')
+def getOne():
+  return json.dumps(collection.find_one({},{'_id':0}))
+
+@api.route('/get_one/pretty')
+def getPrettyOne():
+  incident = json.dumps(collection.find_one({},{'_id':0}),sort_keys=True, indent=2)
+  incident = incident.replace(' ', '&nbsp;')
+  incident = incident.replace('\n', '<br />')
+  return incident
 
 if __name__ == '__main__':
   config = ConfigParser.RawConfigParser()
@@ -8,6 +23,8 @@ if __name__ == '__main__':
   db = client[config.get('database','db')]
   collection = db[config.get('database','coll')]
   
-  print collection.find_one()
+  api.run(debug=True,host=config.get('server','host'),port=config.getint('server','port'))
+  
+
 
                               
