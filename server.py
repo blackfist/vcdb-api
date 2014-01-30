297413439eb088d2ded2e6c9f53ac3d37713ec91
Kevin Thompson <kevin.david.thompson@gmail.com>
2014-01-30 19:45:01 +0000

    trying to add aggregate industries

--- server.py
+++ server.py
@@ -6,6 +6,7 @@ from datetime import datetime
 from bson.son import SON
 from collections import defaultdict
 import apiconstants
+from operator import itemgetter,attrgetter
 api = Flask(__name__)
 
 def addFriendlyCountry(inArray):
@@ -23,6 +24,33 @@ def addFriendlyIndustry(inArray):
     except:
       eachIndustry['friendly_name'] = 'Error'
   return inArray
+
+def aggregateIndustry(inArray):
+  returnArray = [{'_id':'31-33','friendly_name':'Manufacturing','count':0},
+                 {'_id':'44-45','friendly_name':'Retail','count':0},
+                 {'_id':'48-49','friendly_name':'Transportation','count':0}]
+  for eachIndustry in inArray:
+    if eachIndustry['_id'] in ['31','32','33']:
+      returnArray[0]['count'] += eachIndustry['count']
+      continue
+    if eachIndustry['_id'] in ['44','45']:
+      returnArray[1]['count'] += eachIndustry['count']
+      continue
+    if eachIndustry['_id'] in ['48','49']:
+      returnArray[2]['count'] += eachIndustry['count']
+      continue
+    try:
+      eachIndustry['friendly_name'] = apiconstants.industry_remap[eachIndustry['_id']]
+      returnArray.append(eachIndustry)
+    except:
+      eachIndustry['friendly_name'] = 'Error'
+      returnArray.append(eachIndustry)
+    # Neither of these sorting options work properly. The output is sorted, but 
+    # the value of some fields gets set to zero
+    #returnArray = sorted(returnArray,key=itemgetter('count'),reverse=True)
+    returnArray.sort(key=itemgetter('count'),reverse=True)
+  return returnArray
+  
 
 @api.route('/api/get_one')
 def getOne():
@@ -46,11 +74,7 @@ def victims():
   answer['country'] = addFriendlyCountry(country_count['result'])
   industry = collection.aggregate([{'$project':{'pair':{'$substr':['$victim.industry',0,2]}}},{'$group':{'_id':'$pair','count':{'$sum':1}}},{"$sort": SON([("count", -1)])}])
   answer['industry'] = addFriendlyIndustry(industry['result'])
-  for eachIndustry in answer['industry']:
-    try:
-      eachIndustry['friendly_name'] = apiconstants.industry_remap[eachIndustry['_id']]
-    except:
-      eachIndustry['friendly_name'] = 'Error'
+  answer['aggregate_industry'] = aggregateIndustry(industry['result'])
     
   answer = json.dumps(answer)
   resp = Response(answer,status=200, mimetype='application/json')
@@ -80,6 +104,7 @@ def victimByCountry(country_code):
   answer['employe_count'] = employee_count['result']
   answer['country'] = addFriendlyCountry(country['result'])
   answer['industry'] = addFriendlyIndustry(industry['result'])
+  answer['aggregate_industry'] = aggregateIndustry(industry['result'])
   answer['count'] = country['result'][0]['count']
   return json.dumps(answer)
 
